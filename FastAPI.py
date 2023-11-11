feeaad3dcb3ee6174b723af9669943d45d43ae95
itz-Mathankumar <mathankumarv.dev@gmail.com>
2023-11-11 08:59:28 +0530

    add fastapi endpoints

--- FastAPI.py
+++ FastAPI.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
+
 import joblib
+
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -9,17 +10,37 @@ class request_body(BaseModel):
     
     data: str
     
+model = joblib.load('./Models/text_emotion_classifier.pkl')
+    
 @app.post('/predict')
 
 def predict(data: request_body):
      
 	data = list(data.dict().values())
 
-	model = joblib.load('./Models/text_emotion_classifier.pkl')
-
 	emotion = model.predict(data)
 
 	return { 'emotion': emotion[0]}
+
+@app.post('/predict/{emotion}')
+
+def predict_emotion(emotion: str, request_body: request_body):
+    
+    valid_emotions = ['anger', 'disgust', 'fear', 'joy', 'neutral', 'sadness', 'shame', 'surprise']
+    
+    if emotion not in valid_emotions:
+        
+        return {'error': 'Invalid emotion'}
+
+    data = [request_body.data]
+    
+    class_probabilities = model.predict_proba(data)
+    
+    emotion_index = valid_emotions.index(emotion)
+    
+    emotion_score = class_probabilities[0][emotion_index]
+
+    return {emotion: emotion_score}
 
 @app.get('/')
 
